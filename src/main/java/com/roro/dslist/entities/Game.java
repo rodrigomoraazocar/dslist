df4e09b4cbbc8ebe672b2e415eb07c5178f83e0a
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-09-06 19:07:45 -0300

    Populando la Tabla tb_Game

--- src/main/java/com/roro/dslist/entities/Game.java
+++ src/main/java/com/roro/dslist/entities/Game.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 
 
 @Entity //Indica que la clase Game será mapeado y se convierta en tabla en la BD
-@Table(name = "tb_name") //Indica que se llame tb_name y no el nombre de la clase
+@Table(name = "tb_game") //Indica que se llame tb_game y no el nombre de la clase
 public class Game {
 
 	@Id       //Para indicar que el atributo id será el id de la base datos
@@ -23,23 +23,29 @@ public class Game {
 	private Integer year;
 	
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition="TEXT")  //Para que pueda aceptar mas de 255 caracteres (varchar) o sea para que sea tipo TEXT  en la BD
 	private String shortDescription;
+	
+	@Column(columnDefinition="TEXT")
 	private String longDescription;
 
 	//Constructores
 	public Game() {
       }
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score=score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -81,12 +87,21 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
